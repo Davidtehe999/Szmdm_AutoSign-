0f1c18ba28936532a688b8bd3ed2b1c9427ca786
Davidtehe999 <37096177+Davidtehe999@users.noreply.github.com>
2019-01-19 17:42:33 +0800

    加入了cookies
    每次登陆前先检查cookie，如果cookie登陆失败，再走验证登陆。

--- Smzdm_AutoSign_linux.py
+++ Smzdm_AutoSign_linux.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.options import Options
 import sys
 import random
 import time
-
+import json
 
 class Smzdm():
     #初始化
@@ -29,57 +29,75 @@ class Smzdm():
             # 设置屏幕器宽高
             chromeOptions.add_argument("--window-size=1440,750")
             #设置沙盒模式，在无gui的环境中非常重要！！！！
-            #chrome_options.add_argument("--no-sandbox");
+            chromeOptions.add_argument("--no-sandbox")
 
 
     def login(self):
-        self.browser.set_page_load_timeout(20)
+        #self.browser.set_page_load_timeout(20)
         #self.browser.set_script_timeout(20)
         try:
             self.browser.get(self.url)
+
         except:
             print("Loading timeout, pass...")
             self.browser.execute_script("window.stop()")
             print("执行停止")
             pass
+
+        #尝试用cookies登陆
+        try:
+            # 加入cookies
+            self.login_with_cookie()
+            time.sleep(5)
+        except:
+            pass
+
+
+
+        #判断cookies是否过期,方法为检测登陆按钮，如果是签到得积分，则表示登陆失败
         signClick = self.browser.find_element_by_class_name("old-entry")
-        signClick.click()
-        self.browser.implicitly_wait(5.7)
-        time.sleep(3)
+        #登陆失败则重新登陆，过geetest，并且保存最新的cookie
+        if signClick.text == "签到得积分":
+            print("cookies登陆失败，开始过验证登陆")
+            signClick.click()
+            self.browser.implicitly_wait(5.7)
+            time.sleep(3)
 
-        #跳转到登陆的frame
-        self.browser.switch_to.frame("J_login_iframe")
+            #跳转到登陆的frame
+            self.browser.switch_to.frame("J_login_iframe")
 
-        userName = self.browser.find_element_by_id("username")
-        passWord = self.browser.find_element_by_id("password")
-        submit = self.browser.find_element_by_id("login_submit")
-        userName.send_keys(self.myUserName)
-        time.sleep(0.5)
-        passWord.send_keys(self.myPassWord)
-        time.sleep(0.88)
-        submit.click()
+            userName = self.browser.find_element_by_id("username")
+            passWord = self.browser.find_element_by_id("password")
+            submit = self.browser.find_element_by_id("login_submit")
+            userName.send_keys(self.myUserName)
+            time.sleep(0.5)
+            passWord.send_keys(self.myPassWord)
+            time.sleep(0.88)
+            submit.click()
 
-        #有缺口的被打乱成52块的验证图在网页的位置
-        gt_cut_bg = "//div[@class='gt_cut_bg gt_show']/div"
-        #有缺口的被打乱成52块的验证图即将在本地保存的文件名
-        img_file_name_0 = "./images/smzdm/cut_bg.jpg"
-        #把被打乱的验证图根据网页提供坐标还原为正常的图
-        cut_bg = self.get_img(gt_cut_bg, img_file_name_0)
+            #有缺口的被打乱成52块的验证图在网页的位置
+            gt_cut_bg = "//div[@class='gt_cut_bg gt_show']/div"
+            #有缺口的被打乱成52块的验证图即将在本地保存的文件名
+            img_file_name_0 = "./images/smzdm/cut_bg.jpg"
+            #把被打乱的验证图根据网页提供坐标还原为正常的图
+            cut_bg = self.get_img(gt_cut_bg, img_file_name_0)
 
-        #完整的被打乱成52块的验证图在网页的位置
-        get_cut_fullbg_slice = "//div[@class='gt_cut_fullbg gt_show']/div"
-        #完整的被打乱成52块的验证图即将在本地保存的文件名
-        img_file_name_1 = "./images/smzdm/cut_fullbg_slice.jpg"
-        # 把被打乱的验证图根据网页提供坐标还原为正常的图
-        cut_fullbg_slice = self.get_img(get_cut_fullbg_slice, img_file_name_1)
+            #完整的被打乱成52块的验证图在网页的位置
+            get_cut_fullbg_slice = "//div[@class='gt_cut_fullbg gt_show']/div"
+            #完整的被打乱成52块的验证图即将在本地保存的文件名
+            img_file_name_1 = "./images/smzdm/cut_fullbg_slice.jpg"
+            # 把被打乱的验证图根据网页提供坐标还原为正常的图
+            cut_fullbg_slice = self.get_img(get_cut_fullbg_slice, img_file_name_1)
 
-        #计算缺口位置的x坐标
-        loc_x = self.get_gap(cut_bg, cut_fullbg_slice)
+            #计算缺口位置的x坐标
+            loc_x = self.get_gap(cut_bg, cut_fullbg_slice)
 
-        #移动
-        self.move_to_gap(loc_x)
+            #移动
+            self.move_to_gap(loc_x)
 
-        time.sleep(3)
+            time.sleep(5)
+
+
 
 
 
@@ -178,7 +196,9 @@ class Smzdm():
 
                 if not self.is_pixel_equal(image1, image2, x, y):
                     # 返回第一像素不同的位置 x的坐标 即 滑块开始的地方至缺口x位置
+                    print("GAP is:", x)
                     return x
+
 
     def get_track(self, distance):
         '''
@@ -188,33 +208,30 @@ class Smzdm():
         :return: 移动轨迹
         '''
         # 滑块起始位置与图片边缘有点点距离差距 可做点调整
-        distance -= 2.4
+        distance -= 0
         track = []
         current = 0
         mid = distance * 4 / 5
-        t = 0.5
+        t = 0.2
         v = 0
         while current < distance:
             if current < mid:
-                a = random.uniform(1, 0.9)
+                a = random.uniform(4, 5.3)
             else:
-                a = random.uniform(-2,-2.9)
+                a = random.uniform(0,-2.9)
 
             v0 = v
             v = v0 + a * t
             move = v0 * t + 1 / 2 * a * t * t
             current += move
-            print('目前距离:', current)
-            track.append(round(move,1))
+            track.append(round(move))
             #track.append(move)
-            
-            
-        #上面的算法整个track[]之和会与distance有误差（因最后一次while判断），下面是排除误差
-        sumDistance = 0
-        for i in track:
-            sumDistance = sumDistance + i
-        errorDistance = round((distance - sumDistance),3)
+
+        # 上面的算法整个track[]之和会与distance有误差（因最后一次while判断），下面是排除误差
+        sumDistance = sum(track)
+        errorDistance = round((distance - sumDistance), 3)
         track.append(errorDistance)
+
 
         return track
 
@@ -223,19 +240,23 @@ class Smzdm():
         拖动滑块到缺口处
         :return:
         '''
-        #模拟人的拖动，第一阶段拖动滑块超过缺口exceedDistance
-        exceedDistance = round(random.uniform(1,20),2)
+        #tracks = self.get_track(distance + 20)
+
+        # 模拟人的拖动，第一阶段拖动滑块超过缺口exceedDistance
+        exceedDistance = round(random.uniform(10, 20), 2)
         tracks = self.get_track(distance + exceedDistance)
-        
+        print("Distance:", distance)
+        print("exceedDistance:", exceedDistance)
+        print("SumTrack:", sum(tracks))
         #第二阶段滑块往回拖
         tracks_back = []
+        #back = self.get_track(20.1)
         back = self.get_track(exceedDistance)
         for i in back:
             tracks_back.append(i*(-1))
+        tracks_back.append((-4.5))
 
-        print("tracks:", tracks)
-        print("tracks_back:", tracks_back)
-        
+
         # 滑块
         slider_button = self.wait.until(
             EC.element_to_be_clickable((By.XPATH, "//div[@class='gt_slider_knob gt_show']")))
@@ -249,11 +270,13 @@ class Smzdm():
             ActionChains(self.browser).move_by_offset(xoffset=x, yoffset=0).perform()
         time.sleep(0.5)
 
-        #模拟人的拖动，前后犹豫xoffset -+3.5
+        #模拟人的拖动，前后随机犹豫xoffset
         hesitateDistance = round(random.uniform(0,3.5),1)
+        #ActionChains(self.browser).move_by_offset(xoffset=3.5, yoffset=0).perform()
         ActionChains(self.browser).move_by_offset(xoffset=hesitateDistance, yoffset=0).perform()
         time.sleep(0.7)
-        ActionChains(self.browser).move_by_offset(xoffset=hesitateDistance*(-1), yoffset=0).perform()
+        #ActionChains(self.browser).move_by_offset(xoffset=-3.5, yoffset=0).perform()
+        ActionChains(self.browser).move_by_offset(xoffset=(hesitateDistance*(-1)+0.113), yoffset=0).perform()
         time.sleep(0.3)
 
         ActionChains(self.browser).release().perform()
@@ -267,21 +290,50 @@ class Smzdm():
         #签到按钮
         sign = self.browser.find_element_by_class_name("J_punch")
         signState = sign.text
-        print(signState)
         time.sleep(10)
 
         if signState == "签到领积分":#登陆成功
+            # 保存最新的cookie
+
+
             sign.click()
             sign_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             print("smzdm签到成功")
+            self.get_cookie()
+            print("过验证成功，保存最新的cookies")
         elif signState == "签到得积分":#登陆不成功
             print("登陆失败，重新登陆")
-            print(aaaaa)#强制让程序出错，重新try
+            raise RuntimeError("过geetest失败")#强制让程序出错，重新try
         else:
             sign_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+
             print(sign_time)
             print("已经签到")
+            # 保存最新的cookie
+            self.get_cookie()
+            print("过验证成功，保存最新的cookies")
         self.browser.quit()
+
+    #得到cookie
+    def get_cookie(self):
+        #get_cookies()方法得到的是所有的cookies，是一个list
+        cookies = smzdm.browser.get_cookies()
+        # 用dumps()将这个元素变成双引号,type为str，只有str可以保存在txt
+        cookies = json.dumps(cookies)
+        #保存
+        f = open("./smzdm_" + str(self.myUserName)+"_cookie.txt", "w")
+        f.write(cookies)
+
+    #用cookie登陆
+    def login_with_cookie(self):
+        cookie = open("./smzdm_" + str(self.myUserName)+"_cookie.txt").read()
+        #用json.loads()把str变成dict
+        cookie = json.loads(cookie)
+
+        for i in cookie:
+            self.browser.add_cookie(i)
+        self.browser.refresh()
 
 
 
@@ -300,3 +352,6 @@ if(__name__=="__main__"):
             print("错误：", sys.exc_info())
             smzdm.browser.quit()
             print("退出浏览器")
+
+
+
